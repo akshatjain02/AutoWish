a1f2f027f26e603588133061efd69331809db6a6
Akshat Jain <akshatjain0205@yahoo.co.in>
2017-12-27 01:39:52 +0530

    Update FB_Wisher.py

--- FB_Wisher.py
+++ FB_Wisher.py
@@ -6,39 +6,48 @@ import getpass
 
 def openFB():
 	global browser
-	browser = webdriver.Chrome('/media/akshat/Others/Programs/Selenium Webdrivers/chromedriver')
+	try:
+		browser = webdriver.Chrome('/media/akshat/Others/Programs/Selenium Webdrivers/chromedriver')
+	except:
+		print("Couldn't open Chrome browser. Make you have Chrome installed and the path of the chromedriver is correct.")
 	browser.get('https://www.facebook.com/')
 	return
 
 
 def login(fb_ID, password):
-	print('Logging in facebook...')
 	user = browser.find_elements_by_xpath('//*[@id="email"]')
 	user[0].send_keys(fb_ID)
 	user = browser.find_elements_by_xpath('//*[@id="pass"]')
 	user[0].send_keys(password)
 	LOG = browser.find_elements_by_xpath('//*[@id="loginbutton"]')
 	LOG[0].click()
-	print("Login Successfull")
+	#print("Login Successfull")
 	return
 
 
 def wish():
 	browser.get('https://www.facebook.com/events/birthdays/')
-	wish = browser.find_elements_by_xpath('//*[@id="u_0_z"]')
+	wish = browser.find_elements_by_xpath('//*[@class="enter_submit uiTextareaNoResize uiTextareaAutogrow uiStreamInlineTextarea inlineReplyTextArea mentionsTextarea textInput"]')
+	if len(wish)==0:
+		print("Either login failure or no birthdays today")
+		return
 	count = 0;
 	print("Wishing...")
 	for friend in wish:
-		friend.send_keys('Happy birthday! :)')			#Customize the birthday wish
-		friend.send_keys(Keys.RETURN)
+		try:
+			friend.send_keys('Happy birthday! :)')			#Customize the birthday wish
+			friend.send_keys(Keys.RETURN)
+			count += 1
+		except:
+			print("Either already wished or has disabled posts on timeline.")
+			#continue
 		time.sleep(2)
-		count += 1
 	if(count==0):
 		print("None of your friends have their birthday today!")
 	elif(count==1):
 		print("1 person was wished successfully!")
 	else:
-		print(count + " people were wished successfully!")
+		print(str(count) + " people were wished successfully!")
 	return
 
 
@@ -46,6 +55,7 @@ def wish():
 openFB()
 fb_ID = raw_input("Enter facebook ID: ")
 password = getpass.getpass("Password: ")
+print('Logging in facebook...')
 login(fb_ID, password)
 wish()
 
